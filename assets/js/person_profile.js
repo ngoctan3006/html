33043e1654d7debba2bab572961ce77abdeca229
Tân <ngoctana1k2014@gmail.com>
2021-09-13 23:17:32 +0700

    update profile.html

--- assets/js/person_profile.js
+++ assets/js/person_profile.js
@@ -47,3 +47,63 @@ function toggleSkills() {
 skillsHeader.forEach(el => {
     el.addEventListener('click', toggleSkills)
 })
+
+
+// Qualification tabs
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+// Services Modal
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('.services__button')
+const modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach(modalClose => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach(modalView => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+
+// Portfolio swiper
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
