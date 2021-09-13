a2896a1941f3538167c039ef33451ca93b804d6c
Tân <ngoctana1k2014@gmail.com>
2021-09-13 10:16:39 +0700

    update profile.html

--- assets/js/person_profile.js
+++ assets/js/person_profile.js
@@ -27,3 +27,23 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// Accordion skills
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for(let el of skillsContent) {
+        el.className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach(el => {
+    el.addEventListener('click', toggleSkills)
+})
