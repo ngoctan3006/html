2137d6fae59d75cbca8f8af72c728e9acc6bd8d3
Tân <ngoctana1k2014@gmail.com>
2021-09-17 17:44:40 +0700

    update headphone store

--- assets/js/headphone_store/headphone_store.js
+++ assets/js/headphone_store/headphone_store.js
@@ -40,3 +40,31 @@ function scrollHeader(){
     }
 }
 window.addEventListener('scroll', scrollHeader)
+
+// Show scroll up button
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* Scroll section active link */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        const sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
