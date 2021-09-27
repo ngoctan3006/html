cbc70aa2cc6e9c95984f5b42447a9a940b72f8fc
Tân <ngoctana1k2014@gmail.com>
2021-09-27 17:43:55 +0700

    update phone store

--- assets/js/phone_store/main.js
+++ assets/js/phone_store/main.js
@@ -1,2 +1,18 @@
+// Navigation
+const navOpen = document.querySelector('.nav__hamburger')
+const navClose = document.querySelector('.close__toggle')
+const menu = document.querySelector('.nav__menu')
+
+navOpen.addEventListener('click', () => {
+    menu.classList.add('open')
+    document.body.classList.add('active')
+})
+
+navClose.addEventListener('click', (e) => {
+    menu.classList.remove('open')
+    document.body.classList.remove('active')
+    e.preventDefault()
+})
+
 // AOS
 AOS.init()
---
+++ assets/js/phone_store/slider.js
@@ -0,0 +1,1 @@
+new Glide('#glide_1').mount()
