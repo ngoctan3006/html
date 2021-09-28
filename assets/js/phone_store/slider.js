73543d360374b6ba4caef58ec7b9ca6c1d15a200
Tân <ngoctana1k2014@gmail.com>
2021-09-29 00:29:35 +0700

    update phone store: latest products

--- assets/js/phone_store/slider.js
+++ assets/js/phone_store/slider.js
@@ -1,5 +1,6 @@
 // Glide JS Carousel
 const slider1 = document.getElementById('glide_1')
+const slider2 = document.getElementById('glide_2')
 
 // Hero area
 if(slider1) {
@@ -14,3 +15,23 @@ if(slider1) {
     }).mount()
 }
 
+// Latest Product
+if(slider2) {
+    new Glide(slider2, {
+        type: 'carousel',
+        startAt: 0,
+        // autoplay: 3000,
+        hoverpause: true,
+        perView: 4,
+        animationDuration: 800,
+        animationTimingFunc: 'ease-in-out',
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        }
+    }).mount()
+}
