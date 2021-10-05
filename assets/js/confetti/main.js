7bd9d6a7492db38360df3e7ae954c6f8fe886c45
Tân <ngoctana1k2014@gmail.com>
2021-10-05 14:36:01 +0700

    confetti animation

---
+++ assets/js/confetti/main.js
@@ -0,0 +1,19 @@
+const play = document.querySelector('button')
+const svgContainer = document.getElementById('svg')
+
+const animItem = bodymovin.loadAnimation({
+    wrapper: svgContainer,
+    animType: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_rovf9gzu.json',
+})
+
+play.addEventListener('click', () => {
+    svgContainer.classList.remove('hide')
+    animItem.goToAndPlay(0, true)
+})
+
+animItem.addEventListener('complete', () => {
+    svgContainer.classList.add('hide')
+})
